04b8caa1ae78007f8fbb529b77c6fe230d897c07
ansonwan <1563937467@>
2024-03-29 01:06:57 +0800

    Initial commit

---
+++ src/main/java/com/dmx/evaluationplatform/EvaluationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dmx.evaluationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmx/evaluationplatform/EvaluationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmx.evaluationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
